9f29ffc8b61da9df067041250a388758a6ebe242
Pasha Barahimi <pashabarahimi@gmail.com>
2022-12-12 03:10:00 +0330

    Update EX3 change foo program

--- xv6-public/foo.c
+++ xv6-public/foo.c
@@ -3,15 +3,24 @@
 
 #define PROCS_NUM 5
 
-int main() {
-    for (int i = 0; i < PROCS_NUM; ++i) {
+int main()
+{
+    for (int i = 0; i < PROCS_NUM; ++i)
+    {
         int pid = fork();
         if (pid > 0)
             continue;
-        if (pid == 0) {
-            for (long j = 0; j < 1000000000; ++j) {
+        if (pid == 0)
+        {
+            if (i == PROCS_NUM)
+                sleep(8000);
+            else
+                sleep(1000);
+            for (int j = 0; j < 2 * i; ++j)
+            {
                 int x = 1;
-                x++;
+                for (long k = 0; k < 1000000000; ++k)
+                    x++;
             }
             exit();
         }

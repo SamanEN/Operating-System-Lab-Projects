f08a24298777c1348bb7030b7badaf7cf7485ad4
Pasha Barahimi <pashabarahimi@gmail.com>
2023-01-05 01:28:00 +0330

    Add EX4 dining_philosopher problem

--- xv6-public/ulib.c
+++ xv6-public/ulib.c
@@ -104,3 +104,21 @@ memmove(void *vdst, const void *vsrc, int n)
     *dst++ = *src++;
   return vdst;
 }
+
+static uint seed = 1;
+
+void
+srand(uint s)
+{
+  seed = s;
+}
+
+uint
+random(void)
+{
+  seed = seed
+    * 1103515245
+    + 12345
+    % (1 << 31);
+  return seed;
+}
--- xv6-public/user.h
+++ xv6-public/user.h
@@ -50,3 +50,5 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+void srand(uint);
+uint random(void);
